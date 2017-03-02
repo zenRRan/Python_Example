94e2155edb7da77d897681c9f487d4305dd12e9b
zenRRan <824203828@qq.com>
2017-03-02 15:20:25 +0800

    change F_Words.py

--- examples/F_Words.py
+++ examples/F_Words.py
@@ -1,23 +1,30 @@
-#给定一段文本，按照频率降序排列每个文本里面出现的词，并且打印出词频。
-
 file_path = "data.txt"
+# file_path = "data.txt"
 file = open(file_path)
 sent = []
 for line in file.readlines():
-    refresh = line.split(" ")
+    # print(line,end="")
+    refresh = line.strip().split(" ")
     while "" in refresh:
         refresh.remove("")
+    while '\\n' in refresh:
+        refresh.remove('\n')
+    while '\t' in refresh:
+        refresh.remove('\t')
     sent.extend(refresh)
-# print(sent)
+
 l = list(set(sent))
 # print(l)
 value = []
 for word in l:
-    value.extend(str(sent.count(word)))
+    # value.extend(str(sent.count(word)))
+    value.append(sent.count(word))
+    # print(word,sent.count(word))
 for i in range(len(value)):
     value[i] = int(value[i])
 dic = dict(zip(l, value))
-print(dic)
+# print(dic)
 print(sorted(dic.items(), key=lambda d: d[1])[::-1])
+# print(len(dic))
 
 
